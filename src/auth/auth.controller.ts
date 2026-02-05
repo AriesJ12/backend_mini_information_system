8a12b96847affb2b140475bc2dfbd7e1ed199826
AriesJ <ajtagle12@gmail.com>
2026-02-05 15:19:49 +0800

    added dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from 'src/public/public.decorator';
+import { SignInDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,9 +10,9 @@ export class AuthController {
   //TODO validation
   @Public()
   @HttpCode(HttpStatus.OK)
-  @Post()
-  login(@Body() signInDto: Record<string, any>) {
-    return this.authService.login(signInDto.username, signInDto.password);
+  @Post('login')
+  login(@Body() signInDto: SignInDto) {
+    return this.authService.login(signInDto.email, signInDto.password);
   }
 
   @Post()
